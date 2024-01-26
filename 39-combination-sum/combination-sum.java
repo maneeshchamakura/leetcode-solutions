7dac01fa68e35bc1e6f0aef6de44eb5594e7bb46
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 13:13:27 -0500

    Time: 3 ms (39.14%) | Memory: 44.9 MB (15.21%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,22 +1,48 @@
 class Solution {
+    List<Map<Integer, List<List<Integer>>>> cache;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        if (candidates == null || candidates.length == 0) return new ArrayList<>();
+        cache = new ArrayList<>();
+        for (int i=0; i<candidates.length; i++) {
+            cache.add(new HashMap<>());
+        }
         Arrays.sort(candidates);
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(candidates, result, 0, target, new ArrayList<>());
-        return result;
+        List<List<Integer>> res = combinationSum(candidates, 0, target);
+        if (res == null) {
+            return new ArrayList<>();
+        }
+        return res;
     }
-    private void backtrack(int[] nums, List<List<Integer>> result, int start, int remain, List<Integer> chosen) {
-        if (remain < 0) return;
-        else if (remain == 0) {
-            result.add(new ArrayList<>(chosen));
-            return;
-        } else {
-            for (int i=start; i < nums.length; i++) {
-                chosen.add(nums[i]);
-                backtrack(nums, result, i, remain - nums[i], chosen); // not i+1 
-                // because we can choose the same element again
-                chosen.remove(chosen.size() - 1);
+    public List<List<Integer>> combinationSum(int[] nums, int i, int target) {
+        if (i >= nums.length || nums[i] > target) return null;
+        // if (cache.get(i).containsKey(target)) {
+        //     return cache.get(i).get(target);
+        // }
+        // take this number any number of times
+        List<List<Integer>> local = new ArrayList<>();
+        int temp = target;
+        List<Integer> path = new ArrayList<>();
+        while (nums[i] <= target) {
+            target -= nums[i];
+            path.add(nums[i]);
+            if (target == 0) {
+                local.add(new ArrayList<>(path));
+                break;
+            }
+            List<List<Integer>> answers = combinationSum(nums, i+1, target);
+            if (answers != null) {
+                for (List<Integer> answer: answers) {
+                    List<Integer> tempList= new ArrayList<>(path);
+                    tempList.addAll(answer);
+                    local.add(tempList);
+                }
             }
         }
+        // don't take this number
+        List<List<Integer>> answers = combinationSum(nums, i+1, temp);
+        if (answers != null)
+        local.addAll(answers);
+        // cache.get(i).put(target, local);
+        return local;
     }
 }
