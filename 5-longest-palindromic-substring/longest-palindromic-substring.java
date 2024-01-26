7d57064ab4cec7bfaf463e1fa7986273927e2a8b
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 13:25:09 -0500

    Time: 187 ms (27.29%) | Memory: 46.7 MB (9.14%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,32 +1,44 @@
 class Solution {
     public String longestPalindrome(String s) {
+        if (s.length() < 2) return s;
         int n = s.length();
         boolean[][] dp = new boolean[n][n];
+        int start=0, maxLen=1;
+        for (boolean[] subarr: dp) {
+            Arrays.fill(subarr, false);
+        }
         for (int i=0; i<n; i++) {
             dp[i][i] = true;
-        }
-        String result = s.charAt(0)+"";
-        // int max = 1L;
-        // mark the 2 length paldinromes
-        for (int i=0; i<n-1; i++) {
-            dp[i][i+1] = s.charAt(i) == s.charAt(i+1);
-            if (dp[i][i+1]) {
-                result = s.substring(i, i+2);
-            }
-        }        
-        for (int len = 3; len <= n; len++) {
-            for (int i=0; i <= n - len; i++) {
-                int end = i + len - 1;
-                if (s.charAt(i) == s.charAt(end)) {
-                    if (dp[i+1][end-1]) {
-                        if (end - i + 1 > result.length()) {
-                            result = s.substring(i, end+1);
-                        }
-                        dp[i][end] = true;
-                    }
+            if (i+1 != n) {
+                if (s.charAt(i) == s.charAt(i+1)) {
+                    start = i;
+                    maxLen = 2;
+                    dp[i][i+1] = true;
                 }
             }
         }
-        return result;
+        for (int k=3; k <= n; k++) {
+            for (int i=0; i <= n-k; i++) {
+                int end = i + k - 1;
+                if (s.charAt(i) == s.charAt(end) && dp[i+1][end-1]) {
+                    dp[i][end] = true;
+                    if (maxLen < k) {
+                        maxLen = k;
+                        start = i;
+                    }
+                } else {
+                    dp[i][end]= false;
+                }
+            }
+        }
+        return s.substring(start, start+maxLen);
+    }
+    
+    public int expandAroundIndex(String s, int i, int j) {
+        while (i <= j && i >= 0 && j<s.length() && s.charAt(i) == s.charAt(j)) {
+            i--;
+            j++;
+        }
+        return j - i - 1;
     }
 }
