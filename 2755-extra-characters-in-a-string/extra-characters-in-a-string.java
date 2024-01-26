9023e7838fdf5149780b1e6db7a0aaebd4e299ca
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 12:40:32 -0500

    Time: 32 ms (80.83%) | Memory: 45 MB (58.99%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.java
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.java
@@ -1,44 +1,19 @@
 class Solution {
-    static class TrieNode {
-        Map<Character, TrieNode> children;
-        boolean is_word = false;
-        TrieNode() {
-            this.children = new HashMap<>();
-            this.is_word = false;
-        }
-    }
-    public TrieNode buildTrie(String[] dictionary) {
-        TrieNode root = new TrieNode();
-        for (String word: dictionary) {
-            TrieNode temp = root;
-            for(char c: word.toCharArray()) {
-                temp.children.putIfAbsent(c, new TrieNode());
-                temp = temp.children.get(c);
-            }
-            temp.is_word = true;
-        }
-        return root;
-    }
-    public int dp(String s, int n, TrieNode root) {         
-        int[] memo = new int[s.length()+1];
-        for(int i=n-1; i>=0; i--) {
-            int ans = 1+memo[i+1];
-            TrieNode node = root;
-            for(int index = i; index < n; index++) {
-                char c = s.charAt(index);
-                if (!node.children.containsKey(c)) break;
-                node = node.children.get(c);
-                if (node.is_word) {
-                    ans = Math.min(ans, memo[index + 1]);
-                }
-            }
-            memo[i] = ans;
-        }
-        return memo[0];
-    }
     public int minExtraChar(String s, String[] dictionary) {
-        TrieNode root = buildTrie(dictionary);
-        return dp(s, s.length(), root);
+        // bottom up approach
+        int[] dp = new int[51];
+        int n = s.length();
+        for (int i=n-1; i>=0; i--) {
+            dp[i] = 1 + dp[i+1];
+            for(String word: dictionary){
+                // check whether this word exists as substring starting from
+                // the i-th index
+                int cur_len = word.length();
+                if (i+cur_len <= n && s.substring(i, i+cur_len).equals(word)) {
+                    dp[i] = Math.min(dp[i], dp[i+cur_len]);
+                } 
+            }
+        }
+        return dp[0];
     }
-
 }
