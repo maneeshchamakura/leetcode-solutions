79533b695d09dd949bc10cb21d8bf0238b5003d7
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 11:34:41 -0500

    Time: 3 ms (97.12%) | Memory: 45.1 MB (7.27%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,14 +1,25 @@
 class Solution {
     public int[] minOperations(String boxes) {
         int n = boxes.length();
+        int[] leftSum = new int[n];
+        int[] rightSum = new int[n];
+        int opsToLeft = 0;
+        int ballsToLeft = 0;
+        for (int i=0; i<n; i++) {
+            leftSum[i] = opsToLeft;
+            ballsToLeft += (boxes.charAt(i) - '0');
+            opsToLeft += ballsToLeft;
+        }
+        int opsToRight = 0;
+        int ballsToRight = 0;
+        for (int i=n-1; i>=0; i--) {
+            rightSum[i] = opsToRight;
+            ballsToRight += (boxes.charAt(i) - '0');
+            opsToRight += ballsToRight;
+        }
         int[] ans = new int[n];
         for (int i=0; i<n; i++) {
-            char c = boxes.charAt(i);
-            if (c == '0') continue;
-            for (int j=0; j<n; j++) {
-                int steps = Math.abs(j - i);
-                ans[j] += steps;
-            }
+            ans[i] = leftSum[i] + rightSum[i];
         }
         return ans;
     }
