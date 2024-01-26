b0a8d2d6b1aec49c0f303a7a3643a964c248a39b
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 12:00:05 -0500

    Time: 23 ms (98.98%) | Memory: 51.5 MB (48.28%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,28 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        // use the two pointer approach
+        // fix one element and apply two sum logic here
         Arrays.sort(nums);
-        List<List<Integer>> result = new ArrayList<>();
-        for (int i=0; i<nums.length-1; i++) {
-            int num = nums[i];
+        int n = nums.length;
+        for (int i=0; i<n-2; i++) {
+            if (nums[i] > 0) break;
             if (i > 0 && nums[i] == nums[i-1]) continue;
-            int start = i+1;
-            int end = nums.length - 1;
-            while (start < end) {
-                int sum = nums[start] + nums[end] + num;
-                if (sum == 0) {
-                    result.add(new ArrayList<>(Arrays.asList(num, nums[start], nums[end])));
-                    int start_val = nums[start];
-                    // ignore the duplicate values
-                    while (start < end && start_val == nums[start]) start++;
-                    int end_val = nums[end];
-                    while (start < end && end_val == nums[end]) end--;
-                } else if (sum > 0) {
-                    end--;
+            // NOTE: fix i as the first element
+            int first = nums[i];
+            // now use two pointer approach
+            int low = i + 1;
+            int high = n - 1;
+            while (low < high) {
+                int sum = nums[low] + nums[high] + first;
+                if (sum > 0) {
+                    high--;
+                } else if (sum < 0) {
+                    low++;
                 } else {
-                    start++;
+                    results.add(Arrays.asList(first, nums[low], nums[high]));
+                    int low_val = nums[low];
+                    int high_val = nums[high];
+                    // we will ignore the duplicate values here
+                    while (low < high && nums[low] == low_val) low++;
+                    while (low < high && nums[high] == high_val) high--;
                 }
             }
         }
-        return result;
+        return results;
     }
 }
