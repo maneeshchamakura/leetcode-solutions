a1020af7c1b6230297b7f452c38ddad1f786c550
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 13:24:43 -0500

    Time: 0 ms (100.00%) | Memory: 41.2 MB (51.68%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,58 +1,33 @@
 class Solution {
+    private static final int p = 31;
+    private static final int mod = 997;
     public int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) return 0;
+        if (haystack.isEmpty()) return -1;
         if (needle.length() > haystack.length()) return -1;
-        int[] arr = get_array(needle);
-        System.out.println(Arrays.toString(arr)) ;
-        int j=0;
-        for (int i=0; i<haystack.length(); i++) {
-            char c = haystack.charAt(i);
-            if (c == needle.charAt(j)) {
-                j++;
-            } else {
-                if (j > 0) {
-                    while (j > 0) {   
-                        j = arr[j-1];                 
-                        if (c == needle.charAt(j)) {                            
-                            j++;
-                            break;
-                        }
-                    }
-                }
-                if (j == 0) {
-                    if (c == needle.charAt(j)) {
-                        j++;
-                    }
-                }
+        int m = needle.length();
+        int mf = 1;
+        for (int i=1; i<m; i++) {
+            mf = (mf * p)%mod;
+        }
+        int needleHash = hash(needle, 0, m);
+        int hash = hash(haystack, 0, m);
+        for (int i=m; i<=haystack.length(); i++) {
+            if (needleHash == hash && needle.equals(haystack.substring(i-m, i))) {
+                return i-m;
             }
-            if (j == needle.length()) {
-                return i - needle.length() + 1;
-            }
+            if (i < haystack.length()) {
+                hash  = (hash + mod - (mf * haystack.charAt(i-m)%mod))%mod;
+                hash = (hash*p + haystack.charAt(i))%mod;   
+            }            
         }
         return -1;
     }
-
-    int[] get_array(String s) {
-        int n = s.length();
-        int[] arr = new int[n];
-        int j = 0, i=1;
-        while (i < n) {
-            if (s.charAt(j) == s.charAt(i)) {
-                arr[i] = j + 1;
-                j++;
-            } else {
-                while (j > 0) {
-                    j = arr[j-1];
-                    if (s.charAt(j)== s.charAt(i)) {
-                        arr[i] = j + 1;
-                        j++;
-                        break;
-                    }
-                }
-                if (j < 0) arr[i] = 0;
-            }
-            i++;
+    public int hash(String s, int from, int to) {
+        int hash = 0;
+        for (int i=from; i<to; i++) {
+            hash = ((hash * p)% mod + s.charAt(i))%mod;
         }
-        return arr;
+        return hash;
     }
-
 }
