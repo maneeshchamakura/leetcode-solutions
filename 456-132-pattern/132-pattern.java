329e9860cbcd3cc3d0e28ed98da31974c14512ba
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 12:59:43 -0500

    Time: 123 ms (6.84%) | Memory: 84.2 MB (5.29%) - LeetSync

--- 456-132-pattern/132-pattern.java
+++ 456-132-pattern/132-pattern.java
@@ -1,18 +1,21 @@
 class Solution {
     public boolean find132pattern(int[] nums) {
-        if (nums == null || nums.length == 0) return false;
+        // find the pattern
+        if (nums == null || nums.length < 3) return false;
         int n = nums.length;
-        int[] smallLeft = new int[n];
-        smallLeft[0] = nums[0];
-        for (int i=1; i<n; i++) {
-            smallLeft[i] = Math.min(smallLeft[i-1], nums[i]);
+        int[] leftSmall = new int[n];
+        leftSmall[0] = nums[0];
+        for(int i=1; i<n; i++) {
+            leftSmall[i] = Math.min(leftSmall[i-1], nums[i]);
         }
-        TreeSet<Integer> set = new TreeSet<>();
-        for (int i=n-2; i>0; i--) {
+        TreeSet<Integer> set = new TreeSet<>();        
+        for (int i=n-2; i> 0; i--) {
             set.add(nums[i+1]);
             int currentIndex = i;
-            if (smallLeft[i-1] >= nums[currentIndex]) continue;
-            int leftVal = smallLeft[i-1];
+            // currentIndex is the j index
+            if (leftSmall[i-1] >= nums[currentIndex]) continue;
+            int leftVal = leftSmall[i-1];
+            // find the k index
             Integer val = set.lower(nums[currentIndex]);
             if (val != null && val > leftVal) return true;
         }
