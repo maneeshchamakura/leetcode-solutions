7297030d8467a0e959666d9e7275be420a51a0a7
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 13:12:13 -0500

    Time: 0 ms (100.00%) | Memory: 45.9 MB (25.15%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,49 +1,41 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] result = {-1, -1};
-        int index = find_first(nums, target, 0, nums.length-1);
-        if (index == -1) return result;
-        result[0] = index;
-        result[1] = find_last(nums, target, 0, nums.length-1);
-        return result;
-    }
-
-    public int find_first(int[] nums, int target, int low, int high) {
-        if (low > high) return -1;
-        if (low == high) {
-            // last position
-            if (nums[low] == target) return low;
-            return -1;
+        if (nums == null || nums.length == 0) {
+            return new int[]{-1, -1};
         }
-        int mid = low + (high - low)/2;
-        if (nums[mid] == target) {
-            // go to left as we need to look at left for possible occurences
-            return find_first(nums, target, low, mid);
-        } else if (nums[mid] < target) {
-            return find_first(nums, target, mid + 1, high);
-        } else {
-            return find_first(nums, target, low, mid-1);
-        }        
+        return new int[]{firstOccurence(nums, target), lastOccurence(nums, target)};
     }
-
-    public int find_last(int[] nums, int target, int low, int high) {
-        if (low > high) return -1;
-        if (low == high) {
-            // last position
-            if (nums[low] == target) return low;
-            return -1;
-        }
-        int mid = low + (high-low)/2;
-        if (nums[mid] == target) {
-            if (high - low == 1) {
-                if (nums[high] == target) return high;
-                return mid;
+    public int firstOccurence(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low)/2;
+            if (nums[mid] == target) {
+                high = mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
-            return find_last(nums, target, mid, high);
-        } else if (nums[mid] < target) {
-            return find_last(nums, target, mid + 1, high);
-        } else {
-            return find_last(nums, target, low, mid - 1);
         }
+        return (nums[low] == target) ? low: -1;
+    }
+    public int lastOccurence(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low)/2;
+            if (nums[mid] == target) {
+                if (mid == low) {
+                    if (nums[mid] == nums[mid+1]) return mid+1;
+                    return mid;
+                } else {
+                    low = mid;   
+                }                
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            } 
+        }
+        return (nums[low] == target) ? low: -1;
     }
 }
