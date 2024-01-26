74ec6be712c294b604b8e59f5e3fb71536cb2111
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 00:31:35 -0500

    Time: 70 ms (93.35%) | Memory: 22.5 MB (37.57%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.py
+++ 576-out-of-boundary-paths/out-of-boundary-paths.py
@@ -1,14 +1,15 @@
 class Solution:
 
     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
-        moves=[(1, 0), (-1,0), (0, 1), (0,-1)]
-        yo=0
+        delta = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         @cache
-        def f(i, j, moveLeft):
-            if i<0 or i==m or j<0 or j==n: return 1
-            if moveLeft==0: return 0
-            ans=yo
-            for a, b in moves:
-                ans=(ans+f(i+a, j+b, moveLeft-1))%(10**9+7)
+        def dfs(i, j, moves):
+            if i < 0 or i >= m or j < 0 or j >= n:
+                return 1
+            if moves == 0:
+                return 0
+            ans = 0
+            for a, b in delta:
+                ans = (ans + dfs(i + a, j + b, moves-1))%(10**9 + 7)
             return ans
-        return f(startRow, startColumn, maxMove)
+        return dfs(startRow, startColumn, maxMove)
