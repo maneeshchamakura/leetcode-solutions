65a1170f91d54df62a25cab4c118647a098b74bf
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 12:36:21 -0500

    Time: 1 ms (100.00%) | Memory: 42.1 MB (17.14%) - LeetSync

---
+++ 2663-distribute-money-to-maximum-children/distribute-money-to-maximum-children.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int distMoney(int money, int children) {
+        // first condition fails
+        if (money < children) return -1;
+        // satisfy the first condition
+        money -= children;
+        // try satisying the second condition
+        int count_7 = money/7 ;
+        if (count_7 > children) count_7 = children;
+        int rem_money = money - count_7*7;
+        if (rem_money == 0) return count_7;
+         int rem_child = children - count_7;
+        
+        if (rem_money == 3) {
+            // int rem_child = children - count_7;
+            if (rem_child <= 1) {
+                return count_7 - 1;
+            } else {
+                return count_7;
+            }
+        } else {
+            // int rem_child = children - count_7;
+            if (rem_child == 0) {
+                return count_7 - 1;
+            } else {
+                // 1 2 4 5 6 ...
+                return count_7;
+            }
+        }
+    }
+}
