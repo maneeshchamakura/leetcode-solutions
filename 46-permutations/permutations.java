4dd434d2a5ef11e7b0cc020365ad89b9f9e4ae6a
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 14:01:49 -0500

    Time: 1 ms (95.81%) | Memory: 44.4 MB (37.62%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,23 +1,23 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(nums, 0, result);
+        if (nums == null || nums.length == 0) return result;
+        generatePermutations(nums, 0, result);
         return result;
     }
-    public void backtrack(int[] nums, int start, List<List<Integer>> result) {
-        if (start == nums.length) {
-            List<Integer> sol = Arrays.stream(nums).boxed().collect(Collectors.toList());
-            result.add(sol);
+    public void generatePermutations(int[] nums, int index, List<List<Integer>> result) {
+        if (index == nums.length) {
+            List<Integer> path = new ArrayList<>();
+            for (int num: nums) path.add(num);
+            result.add(path);
             return;
         }
-        for (int i=start; i<nums.length; i++) {
-            // swap i and start
-            swap(nums, i, start);
-            backtrack(nums, start + 1, result);
-            swap(nums, i, start);
-        }        
+        for (int i=index; i<nums.length; i++) {
+            swap(nums, i, index);            
+            generatePermutations(nums, index+1, result);
+            swap(nums,i, index);
+        }
     }
-
     public void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
