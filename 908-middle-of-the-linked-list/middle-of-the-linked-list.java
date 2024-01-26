c07abcc803a52a56a49c785715749bf5e38af65a
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 13:52:38 -0500

    Time: 0 ms (100.00%) | Memory: 41.1 MB (23.70%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -10,9 +10,6 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        // use two pointers
-        // move first pointer at 1x speed and other at 2x speed
-        // when second reaches the end first will be at the middle
         ListNode slow=head, fast=head;
         while (fast != null && fast.next != null) {
             slow = slow.next;
