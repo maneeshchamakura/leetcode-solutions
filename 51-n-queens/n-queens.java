b959c7cb8cdbe260122372295989decfe3b2d406
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 13:46:24 -0500

    Time: 1 ms (99.84%) | Memory: 44.4 MB (69.62%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,70 +1,47 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> result = new ArrayList<>();
-        int[][] mat = new int[n][n];
-        char[][] board = new char[n][n];
-        for(char[] sub_arr: board) Arrays.fill(sub_arr, '.');
-        backtrack(mat, board, n, 0, result);
+        List<List<String>>  result = new ArrayList<>();
+        int[] arr = new int[n];
+        Arrays.fill(arr, -1);
+        positionQueens(0, n, new boolean[2*n -1], new boolean[2*n -1], result, arr);
         return result;
     }
-
-    public void backtrack(int[][] mines, char[][] board, int n, int start, List<List<String>> result) {
-        if (start == n) {
-            List<String> ans = new ArrayList<>();
-            for(char[] row: board) ans.add(new String(row));
-            result.add(ans);
+    public void positionQueens(int row, int n, boolean[] diag1, boolean[] diag2, List<List<String>> result, int[] res) {
+        if (row == n) {
+            result.add(createResult(res));
             return;
         }
         for (int i=0; i<n; i++) {
-            if (mines[start][i] == 0) {
-                // put queen
-                board[start][i] = 'Q';
-                put_queen(mines, start, i, n, 1);
-                backtrack(mines, board, n, start + 1, result);
-                put_queen(mines, start, i, n, -1);
-                board[start][i] = '.';
+            if (res[i] != -1 || diag1[row + i]) continue;
+            int x = row - i;
+            if (row - i < 0) {
+                x = Math.abs(row - i) + (n -1);
             }
+            if (diag2[x]) continue;
+            res[i] = row;
+            diag1[row + i] = true;
+            diag2[x] = true;
+            positionQueens(row+1, n, diag1, diag2, result, res);
+            diag1[row + i] = false;
+            diag2[x] = false;
+            res[i] = -1;          
         }
+        
     }
-
-    void put_queen(int[][] mat, int row, int col, int n, int val) {
-        mat[row][col] += val;
-        // go along row and col
+    public List<String> createResult(int[] res) {
+        int n = res.length;
+        char[][] chars = new char[n][n];
         for (int i=0; i<n; i++) {
-            if (i != col) {
-                mat[row][i] += val;
-            }
-            if (i != row) {
-                mat[i][col] += val;
-            }
+            Arrays.fill(chars[i], '.');
         }
-        // go along north east
-        int temp_row = row, temp_col = col;
-        while (temp_row - 1 >= 0 && temp_col + 1 < n) {
-            mat[temp_row - 1][temp_col + 1] += val;
-            temp_row--;
-            temp_col++;
+        for (int i=0; i<res.length; i++) {
+            int row = res[i];
+            chars[row][i] = 'Q';
         }
-        // go along north west
-        temp_row = row; temp_col = col;
-        while (temp_row - 1 >= 0 && temp_col - 1 >= 0) {
-            mat[temp_row - 1][temp_col -1] += val;
-            temp_row--;
-            temp_col--;
+        List<String> result = new ArrayList<>();
+        for(char[] subarr: chars) {
+            result.add(new String(subarr));
         }
-        // go along south west
-        temp_row = row; temp_col = col;
-        while (temp_row + 1 < n && temp_col - 1 >= 0) {
-            mat[temp_row + 1][temp_col - 1] += val;
-            temp_row++;
-            temp_col--;
-        }
-        // go along south east
-        temp_row = row; temp_col = col;
-        while (temp_row + 1 < n && temp_col + 1 < n) {
-            mat[temp_row + 1][temp_col + 1] += val;
-            temp_row++;
-            temp_col++;
-        }        
+        return result;
     }
 }
