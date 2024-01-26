cbc587f00934065fb36a438d78b7228aacbc1949
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 13:45:33 -0500

    Time: 9 ms (30.39%) | Memory: 44.6 MB (16.95%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -7,8 +7,10 @@ class Solution {
             return true;
         }
         if (map.containsKey(index)) return map.get(index);
+        StringBuffer sb = new StringBuffer("");
         for (int i=index; i<s.length(); i++) {
-            if (words.contains(s.substring(index, i+1))) {
+            sb.append(s.charAt(i));
+            if (words.contains(sb.toString())) {
                 if (wordBreak(s, words, i+1, map)) {
                     return true;
                 }
