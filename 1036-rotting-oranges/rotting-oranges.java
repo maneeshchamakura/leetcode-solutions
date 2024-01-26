afc6c5a7243fac8b3f6357e96f2993fe5010daa5
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 13:26:47 -0500

    Time: 1 ms (100.00%) | Memory: 41.8 MB (71.58%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,66 +1,44 @@
 class Solution {
-    static class Pair{
-        int i, j;
-        Pair(int i, int j) {
-            this.i = i;
-            this.j = j;
-        }
-    }
     public int orangesRotting(int[][] grid) {
-        int count = 0;
-        int rows = grid.length, cols = grid[0].length;
-        Queue<Pair> queue = new LinkedList<>();
+        if (grid == null || grid.length == 0) return 0;
+        Queue<Integer> queue = new LinkedList<>();
+        int rows = grid.length;
+        int cols = grid[0].length;
         for (int i=0; i<rows; i++) {
             for (int j=0; j<cols; j++) {
-                int val = grid[i][j];
-                if (val == 2) {
-                    grid[i][j] = 0;
-                    queue.add(new Pair(i, j));
+                if (grid[i][j] == 2) {
+                    queue.add(i*cols + j);
                 }
             }
         }
-        while (!queue.isEmpty()) {
-            int len = queue.size();
-            while (len-- > 0) {
-                Pair p = queue.poll();
-                int i = p.i, j=p.j;
-                // north
-                // i-1 j
-                if(is_valid(i-1, j, rows, cols) && grid[i-1][j] == 1) {
-                    grid[i-1][j] = 0;
-                    queue.add(new Pair(i-1, j));
-                }
-                // west
-                // i j-1
-                if(is_valid(i, j-1, rows, cols) && grid[i][j-1] == 1) {
-                    grid[i][j-1] = 0;
-                    queue.add(new Pair(i, j-1));
-                }
-                // east
-                // i j+1
-                if (is_valid(i, j+1, rows, cols) && grid[i][j+1] == 1) {
-                    grid[i][j+1] = 0;
-                    queue.add(new Pair(i, j+1));
-                }
-                // south
-                // i+1 j
-                if (is_valid(i+1, j, rows, cols) && grid[i+1][j] == 1) {
-                    grid[i+1][j] = 0;
-                    queue.add(new Pair(i+1, j));
+        int time = 0;
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                int code = queue.poll();
+                int r = code / cols;
+                int c = code % cols;
+                for (int k=0; k<4; k++) {
+                    int newR = r + dx[k];
+                    int newC = c + dy[k];
+                    if (newR>= 0 && newR < rows && newC >= 0 && newC < cols && grid[newR][newC] == 1) {
+                        grid[newR][newC] = 2;
+                        queue.add(newR*cols + newC);
+                    }
                 }
             }
-            if (!queue.isEmpty())count++;
+            if (!queue.isEmpty())
+            time++;
         }
-        for(int i=0; i<rows; i++) {
+        for (int i=0; i<rows; i++) {
             for (int j=0; j<cols; j++) {
                 if (grid[i][j] == 1) {
                     return -1;
                 }
             }
         }
-        return count;
-    }
-    public boolean is_valid(int i, int j, int rows, int cols) {
-        return i>= 0 && i < rows && j >= 0 && j < cols;
+        return time;
     }
 }
