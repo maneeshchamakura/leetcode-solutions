f7ad4d27cb965603080e75041547e7a6bb6bdfaf
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 13:58:04 -0500

    Time: 4 ms (87.15%) | Memory: 58.7 MB (14.66%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,18 +1,15 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int low = 1, high = nums.length - 1;
-        while (low < high) {
-            int mid = low + (high - low)/2;
-            int count = 0;
-            for(int num: nums) {
-                if (num <= mid) count++;
-            }
-            if (count > mid) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
+        int slow=0, fast=0;
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+        slow =0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return low;
+        return slow;
     }
 }
