64745556e3b6f837e9c52f24f187e7a804e7606d
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 13:42:39 -0500

    Time: 1 ms (75.90%) | Memory: 46.7 MB (7.01%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,32 +1,20 @@
 class Solution {
     public int trap(int[] height) {
-        // find the amount of water on every bar
         if (height == null || height.length < 3) return 0;
-        int volume = 0, n = height.length;
-        for (int i=0; i<n; i++) {
-            // find max bar on left
-            int maxLeft = maxOnLeft(height, i);
-            // find max bar on right
-            int maxRight = maxOnRight(height, i);
-            volume += Math.min(maxLeft, maxRight) - height[i];
+        int low = 0, high = height.length - 1;
+        int count = 0;
+        int leftMax = height[0], rightMax = height[height.length - 1];
+        while (low <= high) {        
+            leftMax = Math.max(leftMax, height[low]);
+            rightMax = Math.max(rightMax, height[high]);
+            if (leftMax < rightMax) {
+                count += (leftMax - height[low]);
+                low++;                
+            } else {
+                count += (rightMax - height[high]);
+                high--;                
+            }
         }
-        return volume;
-    }
-    
-    public int maxOnLeft(int[] arr, int index) {
-        int leftMax = arr[index];
-        while (index >= 0) {
-            leftMax = Math.max(leftMax, arr[index]);
-            index--;
-        }
-        return leftMax;
-    }
-    public int maxOnRight(int[] arr, int index) {
-        int rightMax = arr[index];
-        while (index < arr.length) {
-            rightMax = Math.max(rightMax, arr[index]);
-            index++;
-        }
-        return rightMax;
+        return count;
     }
 }
