36292332c76aaf0d79bf476d16d7493c17948673
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 13:11:23 -0500

    Time: 28 ms (86.52%) | Memory: 51 MB (79.08%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,34 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> results = new ArrayList<>();
-        // use the two pointer approach
-        // fix one element and apply two sum logic here
+        List<List<Integer>> list = new ArrayList<>();
+        if (nums == null || nums.length < 3) return list;
         Arrays.sort(nums);
         int n = nums.length;
-        for (int i=0; i<n-2; i++) {
-            if (nums[i] > 0) break;
-            if (i > 0 && nums[i] == nums[i-1]) continue;
-            // NOTE: fix i as the first element
-            int first = nums[i];
-            // now use two pointer approach
-            int low = i + 1;
-            int high = n - 1;
-            while (low < high) {
-                int sum = nums[low] + nums[high] + first;
-                if (sum > 0) {
-                    high--;
-                } else if (sum < 0) {
-                    low++;
-                } else {
-                    results.add(Arrays.asList(first, nums[low], nums[high]));
-                    int low_val = nums[low];
-                    int high_val = nums[high];
-                    // we will ignore the duplicate values here
-                    while (low < high && nums[low] == low_val) low++;
-                    while (low < high && nums[high] == high_val) high--;
+        for (int i=0; i <= n-3; i++) {
+            if (i == 0 || nums[i] != nums[i-1]) {
+                int first = nums[i];
+                int sum = first*-1;
+                // find a pair with sum sum
+                int left=i+1, right=n-1;
+                while (left < right) {
+                    if (nums[left] + nums[right] == sum) {
+                        list.add(Arrays.asList(first, nums[left], nums[right]));
+                        while (left+1 < right && nums[left] == nums[left+1]) {
+                            left++;
+                        }
+                        while (right-1 > left && nums[right] == nums[right- 1]) {
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    } else if (nums[left] + nums[right] < sum) {
+                        left++;
+                    } else {
+                        right--;
+                    }
                 }
             }
         }
-        return results;
+        return list;
     }
 }
