d93e46418c6bedc3a4071bb21f3a714c7459e861
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 13:59:44 -0500

    Time: 0 ms (100.00%) | Memory: 42.1 MB (79.23%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,20 +1,20 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        subset_helper(nums, result, new ArrayList<>(), 0);
+        if (nums == null || nums.length == 0) return result;
+        createSubsets(nums, result, 0, new ArrayList<>());
         return result;
     }
-
-    public void subset_helper(int[] nums, List<List<Integer>> result, List<Integer> answer, int index) {
+    
+    public void createSubsets(int[] nums, List<List<Integer>> result, int index, List<Integer> list) {
         if (index == nums.length) {
-            result.add(new ArrayList<>(answer));
+            result.add(new ArrayList<>(list));
             return;
         }
-        // take the element
-        answer.add(nums[index]);
-        subset_helper(nums, result, answer, index+1);
-        // don't take the element
-        answer.remove(answer.size() - 1);
-        subset_helper(nums, result, answer, index + 1);
+        list.add(nums[index]);
+        createSubsets(nums, result, index+1, list);
+        list.remove(list.size() - 1);
+        
+        createSubsets(nums, result, index+1, list);
     }
 }
