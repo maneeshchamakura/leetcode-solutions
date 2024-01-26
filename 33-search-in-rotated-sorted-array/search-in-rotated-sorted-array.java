048b3c8e69cbe3dc9dc38264a410d8553959b4df
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 13:39:43 -0500

    Time: 0 ms (100.00%) | Memory: 41.9 MB (49.50%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,30 +1,23 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        if (nums == null || nums.length == 0) return -1;
-        int n = nums.length;
-        if (nums[0] < nums[n-1]) {
-            int index = Arrays.binarySearch(nums, 0, n, target);
-            if (index < 0) return -1;
-            return index;
-        }
-        int low=0, high=n-1;
+    public int search(int[] nums, int target) {        
+        int low = 0, high=nums.length-1;
         while (low < high) {
             int mid = low + (high - low)/2;
             if (nums[mid] == target) return mid;
-            if (nums[mid] < nums[high]) {
-                if (target > nums[mid] && target <= nums[high]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
-            } else {
+            if (nums[mid] >= nums[low]) {
                 if (target >= nums[low] && target < nums[mid]) {
                     high = mid - 1;
                 } else {
                     low = mid + 1;
                 }
+            } else {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
         }
-        return nums[low] == target ? low: -1;
+        return (nums[low] == target)? low: -1;
     }
 }
