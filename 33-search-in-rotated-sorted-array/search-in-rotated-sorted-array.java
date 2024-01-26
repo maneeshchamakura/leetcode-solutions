848c4096f8899edd80f3252260d36dc7b22c9b02
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 13:08:53 -0500

    Time: 0 ms (100.00%) | Memory: 42.2 MB (22.15%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,33 +1,30 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if (nums.length == 0) return -1;
-        if (nums.length == 1) return (nums[0] == target) ? 0: -1;
+        if (nums == null || nums.length == 0) return -1;
         int n = nums.length;
         if (nums[0] < nums[n-1]) {
-            int index = Arrays.binarySearch(nums, target);
+            int index = Arrays.binarySearch(nums, 0, n, target);
             if (index < 0) return -1;
             return index;
         }
-        int start = 0, end = n - 1;
-        while (start < end) {
-            int mid = start + (end - start)/2;
+        int low=0, high=n-1;
+        while (low < high) {
+            int mid = low + (high - low)/2;
             if (nums[mid] == target) return mid;
-            if (nums[start] <= nums[mid]) {
-                // start to mid is increasing
-                if (target >= nums[start] && target < nums[mid]) {
-                    end = mid - 1;
+            if (nums[mid] < nums[high]) {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
                 } else {
-                    start = mid + 1;
+                    high = mid - 1;
                 }
             } else {
-                // mid to end is increasing
-                if (target > nums[mid] && target <= nums[end]) {
-                    start = mid + 1;
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
                 } else {
-                    end = mid - 1;
+                    low = mid + 1;
                 }
             }
         }
-        return nums[start] == target ? start : -1;
+        return nums[low] == target ? low: -1;
     }
 }
