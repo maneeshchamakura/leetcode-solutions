96d1a648cd11e6a8f6c1ed2d1772c712ba8bb9a2
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 13:57:15 -0500

    Time: 1 ms (99.87%) | Memory: 57 MB (67.97%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,31 +1,10 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        return maxSum(nums, 0, nums.length - 1);
-    }
-    public int maxSum(int[] nums, int left, int right) {
-        if (left == right) {
-            return nums[left];
+        int globalMax = nums[0];
+        for (int i=1; i<nums.length; i++) {
+            nums[i] = Math.max(nums[i], nums[i] + nums[i-1]);
+            globalMax = Math.max(globalMax, nums[i]);
         }
-        int mid = left + (right - left)/2;
-        int leftSum = maxSum(nums, left, mid);
-        int rightSum = maxSum(nums, mid+1, right);
-        int crossSum = crossSum(nums, left, right);
-        return Math.max(Math.max(leftSum, rightSum), crossSum);
-    }
-    public int crossSum(int[] nums, int left, int right) {
-        int mid = left + (right - left)/2;
-        int leftSum = 0;
-        int leftMax = Integer.MIN_VALUE;
-        for (int i=mid; i>=left; i--) {
-            leftSum += nums[i];
-            leftMax = Math.max(leftMax, leftSum);
-        }
-        int rightSum = 0;
-        int rightMax = Integer.MIN_VALUE;
-        for (int i=mid+1; i <= right; i++) {
-            rightSum += nums[i];
-            rightMax = Math.max(rightMax, rightSum);
-        }
-        return leftMax + rightMax;
+        return globalMax;
     }
 }
