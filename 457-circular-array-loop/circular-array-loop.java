3a47f2c03243c2a3cf406a252242b4a6fd9dd2c8
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 13:08:27 -0500

    Time: 2 ms (63.01%) | Memory: 41 MB (30.07%) - LeetSync

--- 457-circular-array-loop/circular-array-loop.java
+++ 457-circular-array-loop/circular-array-loop.java
@@ -1,29 +1,41 @@
 class Solution {
-    public static boolean circularArrayLoop(int[] nums) {
-        int N = nums.length;
-        for(int i=0; i<N; i++){
-            if(checkCycle(nums, i)){
-                return true;
+    public boolean circularArrayLoop(int[] nums) {
+        if (nums == null || nums.length < 2) return false;
+        int n = nums.length;
+        Set<Integer> visited = new HashSet<>();
+        for (int i=0; i<n; i++) {
+            if (nums[i]>= 0 && nums[i] < n) continue;
+            if (nums[i] >= n) nums[i] %= n;
+            else {
+                nums[i] *= -1;
+                nums[i] %= n;
+                nums[i] *= -1;
+            }
+        }
+        for (int i=0; i<n; i++) {
+            if (nums[i] == 0) {
+                visited.add(i);
+                continue;
+            }
+            if (!visited.contains(i)) {
+                Set<Integer> visiting = new HashSet<>();
+                int index = i;
+                boolean isPositive = nums[index] > 0;
+                visiting.add(index);
+                while(true) {
+                    index = (index + nums[index] + n)%n;
+                    if (visiting.contains(index)) return true;
+                    if (visited.contains(index)) break;
+                    if (Math.abs(nums[index])%n == 0) {
+                        visiting.add(index);break;
+                    }                        
+                    if (nums[index] < 0 && isPositive) break;
+                    if (nums[index] > 0 && !isPositive) break;                        
+                    visiting.add(index);
+                }
+                visited.addAll(visiting);
             }
         }
         return false;
     }
-
-    static boolean checkCycle(int[] a, int i){
-        boolean isPositive = a[i] > 0;
-        int n = a.length;
-        boolean[] visited = new boolean[n];
-        while(isPositive == (a[i]>0)){
-            if(visited[i])return true;
-            visited[i] = true;
-            if(a[i]%n == 0)return false;
-            if(a[i] > 0){
-                i = (i + a[i]%n)%n;
-            } else if(a[i] < 0){
-                i = (i - Math.abs(a[i])%n + n)%n;
-            }
-        }
-        return false;
-    }
-
 }
