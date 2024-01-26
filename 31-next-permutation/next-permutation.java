8986fcb61cb4fb8f31c3db55ff129a24fb2a5855
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-01-26 13:55:59 -0500

    Time: 0 ms (100.00%) | Memory: 42.9 MB (40.85%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,42 +1,35 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        if (nums == null || nums.length < 2) {
+        int n = nums.length;
+        int i, j;
+        for(i=n-2; i>=0; i--) {
+            if (nums[i] < nums[i+1]) {
+                break;
+            }
+        }
+        if (i == -1) {
+            reverse(nums, 0, n-1);
             return;
         }
-        // find the first index from right side where arr[i] > arr[i-1]
-        int n = nums.length;
-        int index = -1;
-        for (int i=n-1; i> 0; i--) {
-            if (nums[i] > nums[i-1]) {
-                index = i-1;
+        for (j=n-1; j > i; j--) {
+            if (nums[j] > nums[i]) {
                 break;
             }
         }
-        if (index == -1) {
-            reverse(nums, 0, n-1);
-            return;            
-        }
-        // find the first value from the right side that is greater than arr[i-1]
-        int index2=-1;
-        for (int i=n-1; i > index; i--) {
-            if (nums[i] > nums[index]) {
-                index2 = i;
-                break;
-            }
-        }
-        swap(nums, index, index2);
-        reverse(nums, index+1, n-1);        
-    }
-    void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+        swap(nums, j, i);
+        reverse(nums, i+1, n-1);
     }
     public void reverse(int[] arr, int i, int j) {
-        while (i < j) {
-            swap(arr, i, j);
-            i++;            
-            j--;
+        int start =i, end=j;
+        while (start <= end) {
+            swap(arr, start, end);
+            start++;
+            end--;
         }
+    }
+    public void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
