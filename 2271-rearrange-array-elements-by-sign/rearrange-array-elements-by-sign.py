dd82cf84ab8e0675ee3a817fb1e6e5f94124b950
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-02-13 19:49:57 -0500

    Time: 1001 ms (82.71%) | Memory: 47.3 MB (69.94%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -4,11 +4,11 @@ class Solution:
         result = [0]*n
         positive_i = 0
         negative_i = 1
-        for num in nums:
-            if num > 0:
-                result[positive_i] = num
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                result[positive_i] = nums[i]
                 positive_i += 2
             else:
-                result[negative_i] = num
+                result[negative_i] = nums[i]
                 negative_i += 2
         return result
