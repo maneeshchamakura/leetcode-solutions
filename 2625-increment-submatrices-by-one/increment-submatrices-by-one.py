aa8c0e307f6bcecdcbec9cb64906b8c162a4ee48
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-02-22 23:21:59 -0500

    Time: 1033 ms (80.68%) | Memory: 38.8 MB (70.45%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.py
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.py
@@ -1,15 +1,22 @@
 class Solution:
     def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
-        ans = [[0] * n for _ in range(n)]
-        for r1, c1, r2, c2 in queries:
+        ans = [[0]*n for _ in range(n)]
+        for query in queries:
+            r1, c1 = query[0], query[1]
+            r2, c2 = query[2], query[3]
             ans[r1][c1] += 1
-            if r2 + 1 < n: ans[r2 + 1][c1] -= 1
-            if c2 + 1 < n: ans[r1][c2 + 1] -= 1
-            if r2 + 1 < n and c2 + 1 < n: ans[r2 + 1][c2 + 1] += 1
-        for r in range(n):
-            for c in range(1, n):
-                ans[r][c] += ans[r][c - 1]
-        for r in range(1, n):
-            for c in range(n):
-                ans[r][c] += ans[r - 1][c]
+            if c2 + 1 < n:
+                ans[r1][c2 + 1] -= 1
+            if r2 + 1 < n:
+                ans[r2 + 1][c1] -= 1
+            if r2 + 1 < n and c2 + 1 < n:
+                ans[r2 + 1][c2 + 1] += 1
+        # add the prefix sum along the column
+        for i in range(n):
+            for j in range(1, n):
+                ans[i][j] += ans[i][j-1]
+        for j in range(n):
+            for i in range(1, n):
+                ans[i][j] += ans[i-1][j]
         return ans
+        
