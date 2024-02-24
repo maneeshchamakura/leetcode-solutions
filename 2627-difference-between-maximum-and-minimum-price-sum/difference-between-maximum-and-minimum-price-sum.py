81d99d81bd308df2d9153518a1dd44c9077b0fea
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-02-24 12:12:28 -0500

    Time: 1587 ms (68.29%) | Memory: 59.3 MB (85.37%) - LeetSync

--- 2627-difference-between-maximum-and-minimum-price-sum/difference-between-maximum-and-minimum-price-sum.py
+++ 2627-difference-between-maximum-and-minimum-price-sum/difference-between-maximum-and-minimum-price-sum.py
@@ -1,26 +1,41 @@
+from typing import List
+from collections import defaultdict
+
+
 class Solution:
     def maxOutput(self, n: int, edges: List[List[int]], price: List[int]) -> int:
+        graph = defaultdict(list)
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
 
-        g = defaultdict(list)
-        for a, b in edges: g[a].append(b) ;  g[b].append(a)
+        def dfs(node, parent=-1):
+            current_price = price[node]
+            max_output = (0, current_price, 0)
 
-        def dfs(node1, node2 =-1):
-            p = price[node1]
-            state = (0, p, 0)
+            for neighbor in graph[node]:
+                if neighbor == parent:
+                    continue
 
-            for n in g[node1]:
-                if n == node2: continue
+                neighbor_output = dfs(neighbor, node)
+                combined_output = (
+                    max(
+                        max_output[0],
+                        neighbor_output[0],
+                        max_output[1] + neighbor_output[2],
+                        max_output[2] + neighbor_output[1],
+                    ),
+                    max(max_output[1], neighbor_output[1] + current_price),
+                    max(max_output[2], neighbor_output[2] + current_price),
+                )
 
-                (a1, a2, a3), (b1, b2, b3) = state, dfs(n, node1)
-                
-                state = (max(a1, b1, a2 + b3, a3 + b2),
-                         max(a2, b2 + p),
-                         max(a3, b3 + p))
+                max_output = combined_output
 
-            return state
+            return max_output
 
-        if n <= 2: return sum(price) - min(price)
+        if n <= 2:
+            return sum(price) - min(price)
 
         for node in range(n):
-            if len(g[node]) > 1:
+            if len(graph[node]) > 1:
                 return dfs(node)[0]
