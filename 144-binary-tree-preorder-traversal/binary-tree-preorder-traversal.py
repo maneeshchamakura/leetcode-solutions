f99d65bdde4be2580fec2a2b2169161ad85f3248
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-02-26 13:12:06 -0500

    Time: 34 ms (70.09%) | Memory: 16.6 MB (55.86%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
@@ -6,14 +6,25 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        inorder, preorder, postorder = [], [], []
+        if root == None:
+            return postorder
         stack = []
-        curr = root
-        result = []
-        while curr != None or stack:
-            while curr != None:
-                stack.append(curr)
-                result.append(curr.val)
-                curr = curr.left
-            curr = stack.pop()
-            curr = curr.right
-        return result
+        stack.append((root, 1))
+        while stack:
+            current_node, val = stack.pop()
+            if val == 1:
+                preorder.append(current_node.val)
+            elif val == 2:
+                inorder.append(current_node.val)
+            else:
+                postorder.append(current_node.val)
+            if val != 3:
+                stack.append((current_node, val + 1))
+            if val == 1 and current_node.left:
+                # add the left child if it exists
+                stack.append((current_node.left, 1))
+            if val == 2 and current_node.right:
+                # add the right child if it exists
+                stack.append((current_node.right, 1))
+        return preorder
