de6729131865e161008f4b5a4d885536090d9ce6
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-02-26 13:10:51 -0500

    Time: 35 ms (60.56%) | Memory: 16.7 MB (40.36%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -1,18 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        stack1 = []
-        stack2 = []
-        stack1.append(root)
-        if not root:
-            return []
-        while stack1:
-            curr_node = stack1.pop()
-            stack2.append(curr_node)
-            if curr_node.left:
-                stack1.append(curr_node.left)
-            if curr_node.right:
-                stack1.append(curr_node.right)
-        result = []
-        while stack2:
-            result.append(stack2.pop().val)
-        return result
+        inorder, preorder, postorder = [], [], []
+        if root == None:
+            return postorder
+        stack = []
+        stack.append((root, 1))
+        while stack:
+            current_node, val = stack.pop()
+            if val == 1:
+                preorder.append(current_node.val)
+            elif val == 2:
+                inorder.append(current_node.val)
+            else:
+                postorder.append(current_node.val)
+            if val != 3:
+                stack.append((current_node, val + 1))
+            if val == 1 and current_node.left:
+                # add the left child if it exists
+                stack.append((current_node.left, 1))
+            if val == 2 and current_node.right:
+                # add the right child if it exists
+                stack.append((current_node.right, 1))
+        return postorder
+            
