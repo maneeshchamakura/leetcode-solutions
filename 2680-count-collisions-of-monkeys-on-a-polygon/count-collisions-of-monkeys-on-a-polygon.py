e0cb4d57465a812beedaf0c2120c70d79266447f
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-02-27 17:46:02 -0500

    Time: 34 ms (65.14%) | Memory: 16.6 MB (74.86%) - LeetSync

--- 2680-count-collisions-of-monkeys-on-a-polygon/count-collisions-of-monkeys-on-a-polygon.py
+++ 2680-count-collisions-of-monkeys-on-a-polygon/count-collisions-of-monkeys-on-a-polygon.py
@@ -1,4 +1,14 @@
 class Solution:
     def monkeyMove(self, n: int) -> int:
-        mod = 10**9 + 7
-        return (pow(2, n, mod) - 2)%mod
+        # find the answer 2**n - 2
+        base = 2
+        mod = (10**9) + 7
+        res = 1
+        while n > 0:
+            if n % 2 == 1:
+                res = (res%mod * base%mod)%mod
+            base = (base%mod * base%mod)%mod
+            n //= 2
+        res += (mod - 2)
+        res %= mod
+        return res
