188fb9a854bf757e71a6776792fe41c9f7b723c0
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-03-01 16:53:30 -0500

    Time: 771 ms (24.42%) | Memory: 25.1 MB (85.00%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,16 +1,14 @@
 class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:        
-        m = len(text2)
-        n = len(text1)
-        dp = [[0]*(m+1) for i in range(n + 1)]
-        for i in range(1, m + 1):
-            current_char = text2[i-1]
-            for j in range(1, n + 1):
-                # i belongs to text2
-                # j belongs to text1
-                current_char_2 = text1[j-1]
-                if current_char == current_char_2:
-                    dp[j][i] = 1 + dp[j-1][i-1]
-                else:
-                    dp[j][i] = max(dp[j][i], max(dp[j][i-1], dp[j-1][i]))
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n, m = len(text1), len(text2)
+        dp = [list() for i in range(n + 1)]
+        for i in range(n+1):
+            for j in range(m+1):
+                dp[i].append(0)
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                char1, char2 = text1[i-1], text2[j-1]
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                if char1 == char2:
+                    dp[i][j] = max(dp[i][j], 1 + dp[i-1][j-1])
         return dp[n][m]
