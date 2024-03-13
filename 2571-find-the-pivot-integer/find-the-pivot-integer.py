d8c61233908cb826540f5ca8755e731be269463a
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-03-12 21:37:46 -0400

    Time: 29 ms (97.74%) | Memory: 16.5 MB (92.38%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -1,19 +1,8 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        result = -1
-        low, high = 1, n
-        while low <= high:
-            mid = (low + high) // 2
-            # sum till 1..mid
-            left_sum = (mid*(mid + 1)) // 2
-            # sum from mid..high
-            right_count = n - mid
-            right_sum = (right_count*(right_count + 1)) // 2
-            right_sum += (right_count+1)*mid
-            if left_sum == right_sum:
-                return mid
-            elif left_sum < right_sum:
-                low = mid + 1
-            else:
-                high = mid - 1
+        total_sum = (n*(n+1))//2
+        sqrt_val = floor(sqrt(total_sum))
+        if sqrt_val*sqrt_val == total_sum:
+            return sqrt_val
         return -1
+        
