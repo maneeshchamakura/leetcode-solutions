9a1688fe72bf6a2966ee6acf68a4bcae1ee7d06b
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-03-26 22:19:05 -0400

    Time: 50 ms (5.86%) | Memory: 17.6 MB (22.95%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
@@ -4,29 +4,44 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from functools import cmp_to_key
+from sortedcontainers import SortedList
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        vertical_order = defaultdict(list)
+        nodes_position = {}
+        def traverse(root, x, y):
+            key = (x, y)
+            if root is None:
+                return
+            sorted_list = nodes_position.get(key, SortedList())
+            sorted_list.add(root.val)
+            nodes_position[key] = sorted_list
+            traverse(root.left, x + 1, y - 1)
+            traverse(root.right, x + 1, y + 1)
+        traverse(root, 0, 0)
+        nodes_items = list(nodes_position.items())        
+        def sort_function(key1, key2):
+            x1, y1, x2, y2 = key1[0][0], key1[0][1], key2[0][0], key2[0][1]
+            if y1 == y2:
+                if x1 < x2:
+                    return -1
+                return 1
+            if y1 < y2:
+                return -1
+            return 1
+        nodes_items.sort(key=cmp_to_key(sort_function))
+        print(nodes_items)
         result = []
-        if root == None:
-            return result
-        q = deque()
-        q.append((root, 0))
-        min_val = 0
-        while q:
-            sz = len(q)
-            temp_dict = defaultdict(list)
-            for _ in range(sz):
-                node, index = q.popleft()
-                min_val = min(min_val, index)
-                temp_dict[index].append(node.val)
-                if node.left:
-                    q.append((node.left, index - 1))
-                if node.right:
-                    q.append((node.right, index + 1))
-            for key, val in temp_dict.items():
-                vertical_order[key].extend(sorted(val))
-        while min_val in vertical_order:
-            result.append(vertical_order[min_val])
-            min_val += 1
+        # merge the elements belonging to the same column
+        index = 0
+        n = len(nodes_items)
+        while index < n:
+            current_col = nodes_items[index][0][1]
+            current_list = []
+            current_list.extend(nodes_items[index][1])
+            index += 1
+            while index < n and nodes_items[index][0][1] == current_col:
+                current_list.extend(nodes_items[index][1])
+                index += 1
+            result.append(current_list)
         return result
