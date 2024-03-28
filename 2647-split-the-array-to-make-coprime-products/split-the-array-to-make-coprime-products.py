5c6fb9400c56cf926178991c032fe911cf60758d
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-03-27 23:24:28 -0400

    Time: 3427 ms (27.69%) | Memory: 18.6 MB (66.15%) - LeetSync

--- 2647-split-the-array-to-make-coprime-products/split-the-array-to-make-coprime-products.py
+++ 2647-split-the-array-to-make-coprime-products/split-the-array-to-make-coprime-products.py
@@ -1,26 +1,34 @@
 class Solution:
     def findValidSplit(self, nums: List[int]) -> int:
         freq = Counter()
-        for x in nums: 
-            for p in range(2, isqrt(x)+1): 
-                while x % p == 0: 
+        for x in nums:
+            # find the prime factors of num
+            for p in range(2, isqrt(x) + 1):                   
+                while x % p == 0:
+                    x //= p
                     freq[p] += 1
-                    x //= p 
-            if x > 1: freq[x] += 1
-        ovlp = 0 
+            if x > 1:
+                freq[x] += 1
         prefix = Counter()
-        for i, x in enumerate(nums): 
-            if i <= len(nums)-2: 
-                for p in range(2, isqrt(x)+1): 
-                    if x % p == 0: 
-                        if prefix[p] == 0: ovlp += 1
-                        while x % p == 0: 
+        overlap = 0
+        for i, num in enumerate(nums):
+            if i <= len(nums) - 2:
+                # current is num
+                for p in range(2, isqrt(num) + 1):
+                    if num % p == 0:
+                        if prefix[p] == 0:
+                            overlap += 1
+                        while num % p == 0:
+                            num //= p
                             prefix[p] += 1
-                            x //= p 
-                        if prefix[p] == freq[p]: ovlp -= 1
-                if x > 1: 
-                    if prefix[x] == 0: ovlp += 1
-                    prefix[x] += 1
-                    if prefix[x] == freq[x]: ovlp -= 1
-                if not ovlp: return i 
-        return -1 
+                        if prefix[p] == freq[p]:
+                            overlap -= 1
+                if num > 1:
+                    if prefix[num] == 0:
+                        overlap += 1
+                    prefix[num] += 1
+                    if prefix[num] == freq[num]:
+                        overlap -= 1
+                if not overlap:
+                    return i
+        return -1
