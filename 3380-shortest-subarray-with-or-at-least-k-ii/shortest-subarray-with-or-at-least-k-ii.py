ba992d86acf3c1cab38c4170e952e852853f384a
maneesh <123284194+maneeshchamakura@users.noreply.github.com>
2024-03-30 12:06:16 -0400

    Time: 2725 ms (100.00%) | Memory: 37.8 MB (100.00%) - LeetSync

---
+++ 3380-shortest-subarray-with-or-at-least-k-ii/shortest-subarray-with-or-at-least-k-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
+        def f(count):
+            res = 0
+            for i, c in enumerate(count):
+                if c > 0: res += 1 << i
+                i += 1
+            return res
+        
+        count = [0] * 32
+        start, res = 0, len(nums) + 1
+        for end, num in enumerate(nums):
+            for i in range(32):
+                if num & (1 << i):
+                    count[i] += 1
+            while start <= end and f(count) >= k:
+                for i in range(32):
+                    if nums[start] & (1 << i):
+                        count[i] -= 1
+                res = min(res, end - start + 1)
+                start += 1
+
+        return res if res <= len(nums) else -1
